62750cf272c1c3a4083043886eea5eac470bf6b6
Gaktsu <tjskxo@naver.com>
2026-04-03 18:17:43 +0900

    inference_loop 함수 제거 및 관련 임포트 정리

--- jetson-main/jetson-main/pipeline/__init__.py
+++ jetson-main/jetson-main/pipeline/__init__.py
@@ -1,5 +1,5 @@
 from pipeline.shared_state import SharedState
 from pipeline.capture import capture_loop, start_capture_threads
-from pipeline.inference import inference_loop, start_inference_thread
+from pipeline.inference import start_inference_thread
 from pipeline.recorder import save_loop, start_save_thread
 from pipeline.sensors import start_sensor_threads
